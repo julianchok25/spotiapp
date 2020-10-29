15907cdc6f28e7748c0fbe2de45eb1b91020bb7b
Julian Villegas <julian.villegasplus@gmail.com>
2020-10-28 23:12:14 -0500

    [UPD]search.component: Building search input with its service and create cards in template

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,6 +15,14 @@ export class SpotifyService {
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtist(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQBBavaMWI_NTIjid9Wy8si6Gh7I27_CE9RnWFpH-Ryya7ouMXI1ctN8qde2Khdn4pyXQ8Cd_FDo8QcWys8'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist`, {headers});
 
   }
 }

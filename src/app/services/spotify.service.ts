0cd7eef6842f89b7d64c4e7bf91959e4add41c2c
Julian Villegas <julian.villegasplus@gmail.com>
2020-11-02 16:02:18 -0500

    [UPD]service: Refactor url's api to manage it with a method called getQuery

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,23 +9,23 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
 
   constructor(private http: HttpClient) { }
-
-  getNewReleases() {
+  // Centralizando información
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQA8Cq5KxcWG14onko5CVPL7xnbdz51QTm0k8SOGySlGwX33COnQShj1y5j814tiGWWgWt7UqqYxJB8Rq78'
+      Authorization: 'Bearer BQAUvQUShAosh6_NNwWZ-wfm6zvCXATEdouc7UjdoozJLlgffJEAw3RLhPxnnl9QqzA-g0vwSvgFc1-jjAY'
     });
+    return this.http.get(url, { headers });
+  }
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+  getNewReleases() {
+    return this.getQuery('browse/new-releases')
       .pipe(map( (data: any) => data.albums.items));
   }
 
   getArtist(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQA8Cq5KxcWG14onko5CVPL7xnbdz51QTm0k8SOGySlGwX33COnQShj1y5j814tiGWWgWt7UqqYxJB8Rq78'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist`, {headers})
+    return this.getQuery(`search?q=${termino}&type=artist`)
       .pipe(map((data: any) => data.artists.items));
   }
 }
